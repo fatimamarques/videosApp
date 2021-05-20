dcdc59f815f5424f47e938a14851596e4573eb60
Maria Fatima Baptista Marques <marques.fatimavb@gmail.com>
2021-05-19 22:04:20 -0300

    Adicionamos o componente Alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(public alertController: AlertController) {}
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Deseja realmente favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Sim, favoritar',
+          handler: () => {
+            console.log('Confirm Okay');
+          },
+        },
+      ],
+    });
 
-  constructor() {}
-
+    await alert.present();
+  }
 }
