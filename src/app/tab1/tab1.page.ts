6072b36a928c84f005956eaf73eb004daf3a47ea
Maria Fatima Baptista Marques <marques.fatimavb@gmail.com>
2021-05-24 15:49:12 -0300

    Adicionamos o array de filmes e a interface de filmes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/iFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,54 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  titulo = 'Videos App';
+  listaVideos: IFilme[] = [
+    {
+     nome: 'Mortal Kombat (2021)',
+     lancamento: '15/04/2021',
+     duracao: '1h 50m',
+     classificacao: 76,
+     cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+     generos: ['Ação', 'Fantasia', 'Aventura']
+
+    },
+    {
+      nome: 'Liga da Justiça de Zack Snyder (2021)',
+      lancamento: '18/03/2021',
+      duracao: '4h 2m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
+      generos: ['Ação', 'Aventura', 'Fantasia', 'Ficção científica']
+    },
+
+    {
+      nome: 'Godzilla vs. Kong (2021)',
+      lancamento: '01/04/2021',
+      duracao: '1h 53m',
+      classificacao: 81,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wllzjZxg4ynlAm5xmOICJ2uHOPJ.jpg',
+      generos: ['Ficção científica', 'Ação', 'Drama']
+    },
+    {
+      nome: 'Sem Remorso (2021)',
+      lancamento: '30/04/2021',
+      duracao: '1h 50m',
+      classificacao: 73,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+      generos: ['Ação', 'Thriller', 'Guerra']
+    },
+    {
+      nome: 'Na Mira do Perigo (2021)',
+      lancamento: '18/02/2021',
+      duracao: '1h 48m',
+      classificacao: 74,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wAJniojhHa4oVKa9AxdNwvpmS74.jpg',
+      generos: ['Ação', 'Thriller', 'Crime']
+    }
+
+
+  ];
+
   constructor(public alertController: AlertController, public toastController: ToastController) {}
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
