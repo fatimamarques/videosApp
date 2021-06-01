8c8ce79647789603594a36cd40954ffa495ed3f8
Maria Fatima Baptista Marques <marques.fatimavb@gmail.com>
2021-06-01 15:26:34 -0300

    criamos a página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'mortal-kombat',
-    loadChildren: () => import('./filmes/mortal-kombat/mortal-kombat.module').then( m => m.MortalKombatPageModule)
-  },
-  {
-    path: 'liga-justica',
-    loadChildren: () => import('./filmes/liga-justica/liga-justica.module').then( m => m.LigaJusticaPageModule)
-  },
-  {
-    path: 'godzilla',
-    loadChildren: () => import('./filmes/godzilla/godzilla.module').then( m => m.GodzillaPageModule)
-  },
-  {
-    path: 'sem-remorso',
-    loadChildren: () => import('./filmes/sem-remorso/sem-remorso.module').then( m => m.SemRemorsoPageModule)
-  },
-  {
-    path: 'na-mira-do-perigo',
-    loadChildren: () => import('./filmes/na-mira-do-perigo/na-mira-do-perigo.module').then( m => m.NaMiraDoPerigoPageModule)
-  },
+
   {
     path: 'dados-filme',
     loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
